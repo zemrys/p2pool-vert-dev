b092f6b897a1be46a74f00c5276767d977b29696
James Lovejoy <jameslovejoy1@gmail.com>
2017-06-06 22:33:08 -0400

    Add second p2pool network for small miners on VTC

---
+++ p2pool/networks/vertcoin2.py
@@ -0,0 +1,20 @@
+from p2pool.bitcoin import networks
+
+PARENT=networks.nets['vertcoin']
+SHARE_PERIOD=15 # seconds
+CHAIN_LENGTH=24*60*60//10 # shares
+REAL_CHAIN_LENGTH=24*60*60//10 # shares
+TARGET_LOOKBEHIND=200 # shares
+SPREAD=12 # blocks
+IDENTIFIER='a06e81c82fcaff83'.decode('hex')
+PREFIX='7c3814b6ccdcf984'.decode('hex')
+P2P_PORT=9347
+MIN_TARGET=4
+MAX_TARGET=2**256//2**20 - 1
+PERSIST=False
+WORKER_PORT=9181
+BOOTSTRAP_ADDRS='vtc.alwayshashing.com pool.vtconline.org uk1.vtconline.org'.split(' ')
+ANNOUNCE_CHANNEL='#p2pool-vtc'
+VERSION_CHECK=lambda v: True
+SOFTFORKS_REQUIRED = set(['nversionbips', 'csv', 'segwit'])
+SEGWIT_ACTIVATION_VERSION = 16
