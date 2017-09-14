649807a162c6a01be58a5caa4df1e0b28ab740d3
James Lovejoy <jameslovejoy1@gmail.com>
2017-09-13 23:00:13 -0400

    Don't divide by zero

--- p2pool/bitcoin/stratum.py
+++ p2pool/bitcoin/stratum.py
@@ -74,8 +74,9 @@ class StratumRPCMiningProvider(object):
 
 	# Disconnect miners with large DOA rates to prevent DoS
         res = got_response(header, worker_name, coinb_nonce)
-        if float(len(self.wb._inner.my_doa_share_hashes)) / float(len(self.wb._inner.my_share_hashes)) > 0.60 and len(self.wb._inner.my_share_hashes) > 20:
-            self.transport.loseConnection()
+	if len(self.wb._inner.my_share_hashes) > 20:
+            if float(len(self.wb._inner.my_doa_share_hashes)) / float(len(self.wb._inner.my_share_hashes)) > 0.60:
+                self.transport.loseConnection()
         return res    
 
     def close(self):
